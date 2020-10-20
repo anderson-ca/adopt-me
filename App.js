1383ec265d85f6afc2223030fcc37c433329bd88
anderson <anderson.d.cardoso92@gmail.com>
2020-10-20 20:30:25 +0400

    add Pet component

--- App.js
+++ App.js
@@ -1,8 +1,31 @@
+const Pet = ({name, animal, breed}) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div",
     {},
-    React.createElement("h1", {}, "Adopt Me!")
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Leela",
+      animal: "Dog",
+      breed: "Ausie",
+    }),
+    React.createElement(Pet, {
+      name: "Ada",
+      animal: "Cat",
+      breed: "Mixed",
+    }),
+    React.createElement(Pet, {
+      name: "Droggo",
+      animal: "Dragon",
+      breed: "Anglo-Saxon Red Coat",
+    })
   );
 };
 
