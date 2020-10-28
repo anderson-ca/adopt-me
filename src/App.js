69a365629adad1e76d7f4edd28a31d7267a99afd
anderson <anderson.d.cardoso92@gmail.com>
2020-10-28 21:36:25 +0400

    add router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
+import Router from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details.js";
 
 // import Pet from "./Pet"; // composite component
 const App = () => {
@@ -8,10 +10,13 @@ const App = () => {
     <React.StrictMode>
       <div>
         <h1>Adopt Me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
-);
+  );
 };
 
 render(<App />, document.getElementById("root"));
