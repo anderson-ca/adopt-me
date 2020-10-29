1a4fb42fed986dce0f497ebf38be070b6771bdc4
anderson <anderson.d.cardoso92@gmail.com>
2020-10-29 16:19:44 +0400

    add details class base component.

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,41 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
 
-const Details = () => {
-  return (
-    <div>
-      <h1>This is the details page...</h1>
-    </div>
-  );
-};
+class Details extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading doggo...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    );
+  };
+}
+
 
 export default Details;
